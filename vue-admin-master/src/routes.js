0daa33a7215aa9c5ae3263db740ed2da0fb2d2e0
409694982 <50447803+409694982@users.noreply.github.com>
2019-05-16 16:59:28 +0800

    前端项目重新提交

---
+++ vue-admin-master/src/routes.js
@@ -0,0 +1,78 @@
+import Login from './views/Login.vue'
+import NotFound from './views/404.vue'
+import Home from './views/Home.vue'
+import Main from './views/Main.vue'
+import Table from './views/nav1/Table.vue'
+import Form from './views/nav1/Form.vue'
+import user from './views/nav1/user.vue'
+import Page4 from './views/nav2/Page4.vue'
+import Page5 from './views/nav2/Page5.vue'
+import Page6 from './views/nav3/Page6.vue'
+import echarts from './views/charts/echarts.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    //{ path: '/main', component: Main },
+    {
+        path: '/',
+        component: Home,
+        name: '商品管理',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            { path: '/main', component: echarts, name: '主页', hidden: true },
+            { path: '/table', component: Table, name: '商品目录管理' },
+            { path: '/form', component: Form, name: '商品类型管理' },
+            { path: '/user', component: user, name: '品牌管理' },
+            { path: '/user', component: user, name: '规格选项管理' },
+            { path: '/user', component: user, name: '库存管理' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '销售管理',
+        iconCls: 'fa fa-id-card-o',
+        children: [
+            { path: '/page4', component: Page4, name: '页面4' },
+            { path: '/page5', component: Page5, name: '页面5' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/page6', component: Page6, name: '订单管理' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/page6', component: Page6, name: '内容模块' }
+        ]
+    },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default routes;
