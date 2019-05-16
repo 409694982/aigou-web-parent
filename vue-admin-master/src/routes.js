3d1aec4204c1c00e3eb31aca48c4cee9899a85a7
409694982 <50447803+409694982@users.noreply.github.com>
2019-05-16 23:12:41 +0800

    品牌的增删查

--- vue-admin-master/src/routes.js
+++ vue-admin-master/src/routes.js
@@ -9,6 +9,8 @@ import Page4 from './views/nav2/Page4.vue'
 import Page5 from './views/nav2/Page5.vue'
 import Page6 from './views/nav3/Page6.vue'
 import echarts from './views/charts/echarts.vue'
+import Brand from './views/product/Brand.vue'
+import ProductType from './views/product/ProductType.vue'
 
 let routes = [
     {
@@ -27,15 +29,24 @@ let routes = [
     {
         path: '/',
         component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/main', component: echarts, name: '首页' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
         name: '商品管理',
         iconCls: 'el-icon-message',//图标样式class
         children: [
-            { path: '/main', component: echarts, name: '主页', hidden: true },
-            { path: '/table', component: Table, name: '商品目录管理' },
-            { path: '/form', component: Form, name: '商品类型管理' },
-            { path: '/user', component: user, name: '品牌管理' },
-            { path: '/user', component: user, name: '规格选项管理' },
-            { path: '/user', component: user, name: '库存管理' },
+            { path: '/table', component: Table, name: '商品目录' },
+            { path: '/productType', component: ProductType, name: '商品类型' },
+            { path: '/brand', component: Brand, name: '商品品牌' },
+            { path: '/user', component: Table, name: '商品规格' },
+            { path: '/user', component: Table, name: '商品库存' },
         ]
     },
     {
@@ -46,16 +57,6 @@ let routes = [
         children: [
             { path: '/page4', component: Page4, name: '页面4' },
             { path: '/page5', component: Page5, name: '页面5' }
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '',
-        iconCls: 'fa fa-address-card',
-        leaf: true,//只有一个节点
-        children: [
-            { path: '/page6', component: Page6, name: '订单管理' }
         ]
     },
     {
---
+++ vue-admin-master/src/views/product/ProductType.vue
@@ -0,0 +1,44 @@
+<template>
+    <el-container style="height: 630px">
+        <el-aside width="300px">
+            <el-tree :data="productTypes" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        </el-aside>
+        <el-main>
+
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                productTypes:[],
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                }
+            }
+        },
+        methods:{
+            loadTree(){
+                this.$http.get("/product/productType/tree").then((res)=>{
+                   this.productTypes = res.data;
+                });
+            }
+        },
+        mounted(){
+            this.loadTree();
+        }
+    }
+</script>
+
+<style scoped>
+    .el-aside{
+        border:1px solid #ccc;
+        border-right: none;
+    }
+    .el-main{
+        border:1px solid #ccc;
+    }
+</style>
